43cca5c63d34006d7c6825d7998d1b317e21669d
Alex Basson <alexbasson@gmail.com>
2015-07-04 23:58:55 -0400

    Fetch movies from api and display them

--- SwiftExperiment/Controllers/MoviesViewController.swift
+++ SwiftExperiment/Controllers/MoviesViewController.swift
@@ -4,6 +4,51 @@ public class MoviesViewController: UIViewController {
     public var moviesView: MoviesView! { return self.view as! MoviesView }
 
     public var movies = [Movie]()
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        #if Production
+            let plistName = "Production"
+        #else
+            let pListName = "Test"
+        #endif
+
+        if let
+            pListFilePath = NSBundle.mainBundle().pathForResource(pListName, ofType: "plist"),
+            pList = NSDictionary(contentsOfFile: pListFilePath),
+            urlString = pList["MovieServiceURL"] as? String,
+            apiKey = pList["MovieServiceAPIKey"] as? String,
+            requestString = urlString + "?apikey=" + apiKey as? String,
+            url = NSURL(string: requestString)
+        {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+                if let data = data {
+                    do {
+                        let json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)
+                        if let json = json as? NSDictionary,
+                               moviesJson = json["movies"] as? NSArray {
+                                let movies: NSMutableArray = []
+                                for movieJson in moviesJson {
+                                    if let title = movieJson["title"] as? String {
+                                        movies.addObject(Movie(title: title))
+                                    }
+                                }
+                                self.movies = movies as NSArray as! [Movie]
+                                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                    self.moviesView.tableView?.reloadData()
+                                })
+                        }
+                    } catch {
+                        // handle the error if the json parsing fails
+                    }
+                }
+            }
+            if let task = task {
+                task.resume()
+            }
+        }
+    }
 }
 
 extension MoviesViewController: UITableViewDataSource {
