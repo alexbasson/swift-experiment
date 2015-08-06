e78b8c05dfb1b96bd3c1db162431e68c2026961f
Alex Basson <alexbasson@gmail.com>
2015-08-06 07:36:35 -0400

    Initialize cellPresenters with an empty array in data source

--- SwiftExperiment/Presenters/CellPresenterDataSource.swift
+++ SwiftExperiment/Presenters/CellPresenterDataSource.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 public class CellPresenterDataSource: NSObject {
-  var cellPresenters: [CellPresenter]?
+  var cellPresenters: [CellPresenter] = []
 
   public func display(cellPresenters cellPresenters: [CellPresenter], tableView: UITableView?) -> Void {
     self.cellPresenters = cellPresenters
@@ -16,22 +16,13 @@ public class CellPresenterDataSource: NSObject {
 
 extension CellPresenterDataSource: UITableViewDataSource {
   public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if let cellPresenters = cellPresenters {
-      return cellPresenters.count
-    } else {
-      return 0
-    }
+    return cellPresenters.count
   }
 
   public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-
-    if let cellPresenters = cellPresenters {
-      let cellPresenter = cellPresenters[indexPath.row]
-      let cell = tableView.dequeueReusableCellWithIdentifier(cellPresenter.cellIdentifier(), forIndexPath: indexPath)
-      cellPresenter.present(cell)
-      return cell
-    }
-
-    return UITableViewCell()
+    let cellPresenter = cellPresenters[indexPath.row]
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellPresenter.cellIdentifier(), forIndexPath: indexPath)
+    cellPresenter.present(cell)
+    return cell
   }
 }
