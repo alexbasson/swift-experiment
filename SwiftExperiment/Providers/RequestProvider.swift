2991f472c300a8724fa36e3382b9a760b31f45af
Alex Basson <alexbasson@gmail.com>
2015-08-08 21:04:35 -0400

    Move apikey and url into build configurations

--- SwiftExperiment/Providers/RequestProvider.swift
+++ SwiftExperiment/Providers/RequestProvider.swift
@@ -1,20 +1,15 @@
 import Foundation
 
 public class RequestProvider {
-  #if Production
-  let plistName = "Production"
-  #else
-  let pListName = "Test"
-  #endif
 
   public init() {}
 
   public func getMoviesRequest() -> NSURLRequest? {
     if let
-      pListFilePath = NSBundle.mainBundle().pathForResource(pListName, ofType: "plist"),
-      pList = NSDictionary(contentsOfFile: pListFilePath),
-      urlString = pList["MovieServiceURL"] as? String,
-      apiKey = pList["MovieServiceAPIKey"] as? String,
+      pList = NSBundle.mainBundle().infoDictionary,
+      movieServiceDict = pList["Movie service"] as? NSDictionary,
+      urlString = movieServiceDict["URL"] as? String,
+      apiKey = movieServiceDict["APIKey"] as? String,
       requestString = urlString + "?apikey=" + apiKey as? String,
       url = NSURL(string: requestString)
     {
